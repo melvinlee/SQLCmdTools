ee049850a11715b7ca54322376d07b49a9b09aac
Lee Chin Yong <melvinlee@hotmail.com>
2017-12-30 09:07:16 +0800

    test: added module unittest.

---
+++ SqlCmdTools.Module.Tests.ps1
@@ -0,0 +1,64 @@
+﻿$currentPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+$functionPath = "$currentPath\Functions\" 
+
+$module = 'SqlCmdTools'
+
+Describe "$module Module Tests"  {
+
+  Context 'Module Setup' {
+    It "has the root module $module.psm1" {
+      "$currentPath\$module.psm1" | Should Exist
+    }
+
+    It "has the a manifest file of $module.psm1" {
+      "$currentPath\$module.psd1" | Should Exist
+      "$currentPath\$module.psd1" | Should FileContentMatch "$module.psm1"
+    }
+
+    It "$module is valid PowerShell code" {
+      $psFile = Get-Content -Path "$currentPath\$module.psm1" `
+                            -ErrorAction Stop
+      $errors = $null
+      $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+      $errors.Count | Should Be 0
+    }
+  }
+
+  $functions = ('Invoke-SqlCmdScript'
+               )
+
+  foreach ($function in $functions)
+  {
+  
+    Context "Test Function $function" {
+      
+      It "$function.ps1 should exist" {
+        "$currentPath\Functions\$function.ps1" | Should Exist
+      }
+    
+      It "$function.ps1 should be an advanced function" {
+        "$functionPath\$function.ps1" | Should FileContentMatch 'function'
+        "$functionPath\$function.ps1" | Should FileContentMatch 'cmdletbinding'
+        "$functionPath\$function.ps1" | Should FileContentMatch 'param'
+      }
+      
+      It "$function.ps1 should contain Write-Verbose blocks" {
+        "$functionPath\$function.ps1" | Should FileContentMatch 'Write-Verbose'
+      }
+    
+      It "$function.ps1 is valid PowerShell code" {
+        $psFile = Get-Content -Path "$functionPath\$function.ps1" `
+                              -ErrorAction Stop
+        $errors = $null
+        $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+        $errors.Count | Should Be 0
+      }
+
+    
+    } # Context "Test Function $function"
+  
+  } # foreach ($function in $functions)
+
+}
+
+
